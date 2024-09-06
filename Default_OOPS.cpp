4ac5c6a5d07fbb78c323befa5e07203f4bbf3029
Sagar <56674688+sagar-tan@users.noreply.github.com>
2024-09-06 09:09:33 +0530

    Created a file Default_OOPS.cpp containing program to create a default cosntructor

---
+++ Default_OOPS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace as std;
+class Wall{
+private:
+double length; 
+double breadth;
+public:
+Wall(){
+  length = 0;
+  breadth = 0;
+  double length, breadth;
+  cout << "Enter the Length: ";
+  cin >> length;
+  count << "Enter the breadth";
+  cin >> breadth
+  double area = length*breadth;
+  cout >> "Area = " << area << endl;
+}
+};
+int main(){
+  Wall wall_Obj;
+  return 0;
+}
